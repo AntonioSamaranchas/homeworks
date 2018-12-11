1f505f83c3f49d26eae9952e1107754264904085
AntonioSamaranchas <ma.ai.87@yandex.ru>
2018-12-11 19:22:38 +0300

    решение задачи Виджит погоды д.р.5

--- xhr/weather-widget/js/main.js
+++ xhr/weather-widget/js/main.js
@@ -1,7 +1,24 @@
 const request = new XMLHttpRequest();
-request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
+/*request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', false);
 request.send();
 if (request.status === 200) {
   const response = JSON.parse(request.responseText);
   setData(response);
+}*/
+request.addEventListener('load', onLoad);
+request.addEventListener('error', onError);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather', true);
+request.send();
+
+function onLoad() {
+  if (request.status === 200) {
+    const response = JSON.parse(request.responseText);
+    setData(response);
+  } else {
+    alert(`Ответ ${request.status}: ${request.statusText}`);
+  }
+}
+
+function onError() {
+  alert('Сервис недоступен');
 }

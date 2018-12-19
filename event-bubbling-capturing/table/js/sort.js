05401385be55992eac69af82331d2817fa517998
AntonioSamaranchas <ma.ai.87@yandex.ru>
2018-12-20 01:10:57 +0300

    решение задачи Сортировка таблицы д.р.8

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,16 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (!event.target.classList.contains('prop__name')) {
+    return;
+  }
+
+  if (event.target.dataset.propName !== event.currentTarget.dataset.sortBy) {
+    window.dir = undefined;
+  }
+
+  event.currentTarget.dataset.sortBy = event.target.dataset.propName;
+  event.currentTarget.firstElementChild.dataset.dir = window.dir ? -window.dir : 1;
+  window.dir = Number(event.currentTarget.firstElementChild.dataset.dir);
+  sortTable(event.target.dataset.propName, window.dir);
 }

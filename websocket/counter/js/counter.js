c8c7fddd1465f9a5e39321b10a8f889576c15b30
morozov.ai <morozov.ai@pecom.local>
2018-12-25 13:51:09 +0300

    решение задачи Счетчик открытых соединений д.р.10

--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function connection() {
+  const counter = document.querySelector('.counter');
+  const errorsСounter = document.querySelector('.errors');
+
+  const ws = new WebSocket('wss://neto-api.herokuapp.com/counter');
+  ws.addEventListener('message', messages);
+
+  function messages() {
+    try {
+      const mess = JSON.parse(event.data);
+      counter.innerHTML = mess.connections;
+      errorsСounter.value = mess.errors;
+    } catch (e) {
+      errorsСounter.value = 'invalid format';
+    }
+  }
+
+  function closeConnection() {
+    ws.onclose = function() {};
+    ws.close(1000, 'ок');
+  }
+
+  window.addEventListener('beforeunload', closeConnection);
+}
+
+connection();

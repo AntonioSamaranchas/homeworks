53cc2c54d652d2e968487c3226be7d5f1929ee42
morozov.ai <morozov.ai@pecom.local>
2018-12-25 16:41:01 +0300

    решение задачи Шарики д.р.10

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+const canvas = document.querySelector('canvas');
+const coord =  {};
+
+function sendClick() {
+  coord.x = event.pageX;
+  coord.y = event.pageY;
+  ws.send(JSON.stringify(coord));
+}
+
+canvas.addEventListener('click', sendClick);
+showBubbles(ws);

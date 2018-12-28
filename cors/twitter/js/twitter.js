5a72d658a15d630218b1af4b5352c29cb19777fa
AntonioSamaranchas <ma.ai.87@yandex.ru>
2018-12-28 18:01:37 +0300

    решение задачи Окно твиттера д.р.11

--- cors/twitter/js/twitter.js
+++ cors/twitter/js/twitter.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const url = 'https://neto-api.herokuapp.com/twitter/jsonp';
+const container = document.querySelector('.container');
+const selectors = [
+  '[data-wallpaper]', 
+  '[data-username]', 
+  '[data-description]',
+  '[data-pic]',
+  '[data-tweets]',
+  '[data-followers]',
+  '[data-following]'
+];
+
+function loadData() {
+  const functionName = 'parser';
+  return new Promise((done, fail) => {
+    window[functionName] = done;
+    const script = document.createElement('script');
+    script.src = `${url}?jsonp=${functionName}`;
+    document.body.appendChild(script);
+  });
+}
+
+function showData(result) {
+  const items = Array.from(container.querySelectorAll(selectors.join()));
+  for (const key in result) {
+    const field = items.find(item => {return item.dataset[key] ===''});
+    if (field) {
+      if (field.tagName === 'IMG') {
+        field.src = result[key];
+      } else {
+        field.textContent = result[key];
+      }
+    }
+  }
+}
+
+
+loadData().then(showData).catch(() => console.log('Не удалось получить данные!'));

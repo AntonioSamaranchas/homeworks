fae9dfc0e27aadc72633b9db28eceef2c1b164a3
morozov.ai <morozov.ai@pecom.local>
2019-01-10 18:40:53 +0300

    решение задачи Интерне-магазин д.р.13

--- dom-from-zero/store/js/dom.js
+++ dom-from-zero/store/js/dom.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function createElement(node) {
+  if (typeof node === 'string') {
+    return document.createTextNode(node);
+  }
+  
+  const element = document.createElement(node.name);
+  if (node.props !== null) {
+    Object.keys(node.props).forEach(i => element.setAttribute(i, node.props[i]));
+  }
+  if (typeof node.childs === 'string') {
+    element.textContent = node.childs;
+  } else if (node.childs instanceof Array) {
+    node.childs.forEach(child => element.appendChild(createElement(child)));
+  }
+  return element;
+}

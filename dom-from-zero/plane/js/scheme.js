96468c579fe2f49eb1ecb8f37e33f8c3b4171abf
morozov.ai <morozov.ai@pecom.local>
2019-01-11 19:45:07 +0300

    решение задачи Самолет д.р.13

--- dom-from-zero/plane/js/scheme.js
+++ dom-from-zero/plane/js/scheme.js
@@ -34,15 +34,64 @@ function addDef() {
   seatMapDiv.appendChild(noFly);
 }
 
+function btnUnlock() {
+  btnSetFull.removeAttribute('disabled');
+  btnSetEmpty.removeAttribute('disabled');
+}
+
+function allBooking() {
+  event.preventDefault();
+  Array.from(seatMapDiv.querySelectorAll('div.seat')).forEach(seat => {
+    seat.classList.remove('half');
+    seat.classList.add('adult');
+  });
+  updateTotal();
+}
+
+function cancelAllBooking() {
+  event.preventDefault();
+  Array.from(seatMapDiv.querySelectorAll('div.seat')).forEach(seat => seat.classList.remove('adult', 'half'));
+  updateTotal();
+}
+
+function updateTotal() {
+  const adult = seatMapDiv.querySelectorAll('div.seat.adult').length;
+  const half = seatMapDiv.querySelectorAll('div.seat.half').length;
+  totalAdult.textContent = adult;
+  totalHalf.textContent = half;
+  totalPax.textContent = adult + half;
+}
+
+function OnClickSeat() {
+  if (event.currentTarget.classList.contains('adult') || event.currentTarget.classList.contains('half')) {
+    event.currentTarget.classList.remove('adult', 'half');
+  } else {
+    event.currentTarget.classList.toggle('adult', !event.altKey);
+    event.currentTarget.classList.toggle('half', event.altKey);
+  }
+  updateTotal();
+}
+
+function ininOnClickSeat() {
+  Array.from(seatMapDiv.querySelectorAll('div.seat')).forEach(seat => seat.addEventListener('click', OnClickSeat));  
+}
+
 function loadSheme() {
   event.preventDefault();
 
   fetch('https://neto-api.herokuapp.com/plane/' + acSelect.value)
     .then(res => res.json())
-    .then(showSheme);
+    .then((res) => {
+      showSheme(res);
+      btnUnlock();
+      updateTotal();
+      ininOnClickSeat();
+    });
 
   function showSheme(data) {
-    const rows = data.scheme.map(seats => createRow(seats, data.letters4, data.letters6));
+    const rows = data.scheme.map(function (seats, i) {
+      return createRow(i + 1, seats, data.letters4, data.letters6);
+    });
     const fragment = rows.reduce((fragment, row) => {
       fragment.appendChild(row);
       return fragment;
@@ -52,8 +101,92 @@ function loadSheme() {
     seatMapDiv.appendChild(fragment);
   }
 
-  function createRow(seats, letters4, letters6) {
-    return seats;
+  function createRow(numRow, seats, letters4, letters6) {
+    if(seats === 0) {
+      return getFragmentNull();
+    } else if (seats === 4) {
+      return getFragmentFourSeat();
+    } else {
+      return getFragmentSeat();
+    }
+
+    function createCommon() {
+      const h2 = document.createElement('h2');
+      h2.textContent = numRow;
+      const xs1 = document.createElement('div');
+      xs1.className = "col-xs-1 row-number";
+      xs1.appendChild(h2);
+      const row = document.createElement('div');
+      row.className = "row seating-row text-center";
+      row.appendChild(xs1);
+      return row;
+    }
+
+    function getFragmentNull() {
+      const row = createCommon();
+      
+      for (let i = 0; i < 2; i++) {
+        const xs5 = document.createElement('div');
+        xs5.className = "col-xs-5";
+        for (let j = 0; j < 3; j++) {
+          const noSeat = document.createElement('div');
+          noSeat.className = "col-xs-4 no-seat";
+          xs5.appendChild(noSeat);
+        }
+        row.appendChild(xs5);
+      }
+      return row;
+    }
+
+    function getFragmentFourSeat() {
+      const row = createCommon();
+      
+      let k = 0;
+      for (let i = 0; i < 2; i++) {
+        const xs5 = document.createElement('div');
+        xs5.className = "col-xs-5";
+        for (let j = 0; j < 3; j++) {
+          if ((i === 0 && j === 0) || (i === 1 && j === 2)) {
+            const noSeat = document.createElement('div');
+            noSeat.className = "col-xs-4 no-seat";
+            xs5.appendChild(noSeat);
+            continue;  
+          }
+          const xs4 = document.createElement('div');
+          xs4.className = "col-xs-4 seat";
+          const label = document.createElement('span');
+          label.className = "seat-label";
+          label.textContent = letters4[k];
+          xs4.appendChild(label);
+          xs5.appendChild(xs4);
+          k++;
+        }
+        row.appendChild(xs5);
+      }
+      return row;  
+    }
+
+    function getFragmentSeat() {
+      const row = createCommon();
+      
+      let k = 0;
+      for (let i = 0; i < 2; i++) {
+        const xs5 = document.createElement('div');
+        xs5.className = "col-xs-5";
+        for (let j = 0; j < 3; j++) {
+          const xs4 = document.createElement('div');
+          xs4.className = "col-xs-4 seat";
+          const label = document.createElement('span');
+          label.className = "seat-label";
+          label.textContent = letters6[k];
+          xs4.appendChild(label);
+          xs5.appendChild(xs4);
+          k++;
+        }
+        row.appendChild(xs5);
+      }
+      return row;
+    }
   }
 }
 
@@ -64,3 +197,5 @@ acSelect.addEventListener('input', addDef);
 acSelect.addEventListener('input', init);
 acSelect.addEventListener('input', changeFly);
 btnSeatMap.addEventListener('click', loadSheme);
+btnSetFull.addEventListener('click', allBooking);
+btnSetEmpty.addEventListener('click', cancelAllBooking);

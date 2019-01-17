89833aead1335053c94fca19bb0a8469edc4befb
morozov.ai <morozov.ai@pecom.local>
2019-01-17 18:25:33 +0300

    решение задачи draw д.р. Comet

--- comet/draw/js/socket.js
+++ comet/draw/js/socket.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function onLoad() {
+  const ws = new WebSocket('wss://neto-api.herokuapp.com/draw');
+
+  function paint() {
+    const ctx = event.target.getContext('2d');
+    const image = ctx.getImageData(0, 0, event.target.width, event.target.height);
+    const binary = Uint8Array.from(image.data);
+    ws.send(binary.buffer); 
+  }
+
+  function closeConnection() {
+    if (ws) {
+      ws.onclose = function() {};
+      ws.close(1000, 'ок');
+    }
+  }
+
+  window.editor.addEventListener('update', paint);
+  window.addEventListener('beforeunload', closeConnection);
+}
+
+window.addEventListener('load', onLoad, false);

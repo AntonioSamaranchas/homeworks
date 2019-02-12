48a34d0a068dd275a4be8dae18e156550d7b3a0e
AntonioSamaranchas <ma.ai.87@yandex.ru>
2019-02-12 23:32:50 +0300

    решение задачи t-m-c д.р. DnD

--- drag-and-drop/tracking-mouse-cursor/js/eye.js
+++ drag-and-drop/tracking-mouse-cursor/js/eye.js
@@ -1,55 +1,66 @@
 'use strict';
 
-function tracking(container) {
-  const leftParent = container.querySelector('.cat_position_for_left_eye');
-  const rightParent = container.querySelector('.cat_position_for_right_eye');
-  const leftEye = container.querySelector('.cat_eye_left');
-  const rightEye = container.querySelector('.cat_eye_right');
-  const bodyWidth = document.documentElement.clientWidth;
-  const bodyHeight = document.documentElement.clientHeight;
+const leftParent = document.querySelector('.cat_position_for_left_eye');
+const rightParent = document.querySelector('.cat_position_for_right_eye');
+const leftEye = document.querySelector('.cat_eye_left');
+const rightEye = document.querySelector('.cat_eye_right');
 
-  const trackingLeftEye = throttle((x, y) => {
-    if (leftParent && leftEye) {
-      let [lft, tp] = calculatePercent(x, y);
-      leftEye.style.left = 100 + '%';
-      leftEye.style.top = 100 + '%';
-    }
-  });
-
-  const trackingRightEye = throttle((x, y) => {
-    if (rightParent && rightEye) {
-      let [lft, tp] = calculatePercent(x, y);
-      rightEye.style.left = lft + '%';
-      rightEye.style.top = tp + '%';
-    }
-  });
-
-  function throttle(callback) {
-    let isWaiting = false;
-    return function () {
-      if (!isWaiting) {
-        callback.apply(this, arguments);
-        isWaiting = true;
-        requestAnimationFrame(() => {
-          isWaiting = false;
-        });
-      }
-    };
+function getPosition(el) {
+  const pos = el.getBoundingClientRect();
+  return {
+    left: pos.left,
+    top: pos.top,
   }
-
-  function calculatePercent(x, y) {
-    return [
-      Math.round(100 * x / bodyWidth),
-      Math.round(100 * y / bodyHeight)
-    ]
-  }
-
-  document.addEventListener('mousemove', event => trackingLeftEye(event.pageX, event.pageY));
-  document.addEventListener('mousemove', event => trackingRightEye(event.pageX, event.pageY));
-  document.addEventListener('touchmove', event => trackingLeftEye(event.touches[0].pageX, event.touches[0].pageY));
-  document.addEventListener('touchmove', event => trackingRightEye(event.touches[0].pageX, event.touches[0].pageY));
-  
 }
 
-const cats = document.querySelectorAll('.cat');
-Array.from(cats).forEach(item => tracking(item));
+function trackingLeftEye(x, y) {
+  if (leftParent && leftEye) {
+    const pos = getPosition(leftParent);
+    let left = x - pos.left;
+    let top = y - pos.top;
+    [left, top] = checkLimit(left, top, leftEye, leftParent);
+
+    leftEye.style.left = `${left}px`;
+    leftEye.style.top = `${top}px`;
+  }
+};
+
+function trackingRightEye(x, y) {
+  if (rightParent && rightEye) {
+    const pos = getPosition(rightParent);
+    let left = x - pos.left;
+    let top = y - pos.top;
+    [left, top] = checkLimit(left, top, rightEye, rightParent);
+
+    rightEye.style.left = `${left}px`;
+    rightEye.style.top = `${top}px`;
+  }
+};
+
+function checkLimit(left, top, eye, parent) {
+  const maxX = parent.offsetWidth;
+  const maxY = parent.offsetHeight;
+
+  if(left < 0) {
+    left = 0;
+  }
+
+  if(top < 0) {
+    top = 0;
+  }
+  
+  if(left + eye.offsetWidth > maxX) {
+    left = maxX - eye.offsetWidth;
+  }
+
+  if(top + eye.offsetHeight > maxY) {
+    top = maxY - eye.offsetHeight;
+  }
+  return [left, top];
+}
+
+document.addEventListener('mousemove', event => trackingLeftEye(event.pageX, event.pageY));
+document.addEventListener('mousemove', event => trackingRightEye(event.pageX, event.pageY));
+document.addEventListener('touchmove', event => trackingLeftEye(event.touches[0].pageX, event.touches[0].pageY));
+document.addEventListener('touchmove', event => trackingRightEye(event.touches[0].pageX, event.touches[0].pageY));
+
